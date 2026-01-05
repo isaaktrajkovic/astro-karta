33346ea45d850923f9b6df3712a1c7a97a4a3f2e
isaaktrajkovic <44225465+isaaktrajkovic@users.noreply.github.com>
2026-01-05 13:05:27 +0100

    Commit 05.01.2026_
    Commit 1.0

--- vite.config.ts
+++ vite.config.ts
@@ -15,4 +15,10 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {
+    esbuild: {
+      drop: ['debugger'],
+      minify: true,
+    },
+  },
 }));
